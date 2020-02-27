8622684468c72e3211d2376eba9dad080eacfeb6
Antoine <antoine.vadot@misakey.com>
2020-02-27 16:39:28 +0100

    fix: remove virtualized list from request

--- src/components/screens/DPO/Service/Requests/List/index.js
+++ src/components/screens/DPO/Service/Requests/List/index.js
@@ -8,14 +8,9 @@ import API from '@misakey/api';
 import { OPEN, REOPEN } from 'constants/databox/status';
 
 import isNil from '@misakey/helpers/isNil';
-import noop from '@misakey/helpers/noop';
 import objectToCamelCase from '@misakey/helpers/objectToCamelCase';
 import objectToSnakeCase from '@misakey/helpers/objectToSnakeCase';
 import { getStatus } from '@misakey/helpers/databox';
-
-import InfiniteLoader from 'react-virtualized/dist/commonjs/InfiniteLoader';
-import AutoSizer from 'react-virtualized/dist/commonjs/AutoSizer';
-import VirtualizedList from 'react-virtualized/dist/commonjs/List';
 
 import useFetchEffect from '@misakey/hooks/useFetch/effect';
 
@@ -27,7 +22,6 @@ import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItem from '@material-ui/core/ListItem';
-import Skeleton from '@material-ui/lab/Skeleton';
 import BoxSection from '@misakey/ui/Box/Section';
 import Empty from 'components/dumb/Box/Empty';
 import ScreenAction from 'components/dumb/Screen/Action';
@@ -48,9 +42,6 @@ const ENDPOINTS = {
   },
 };
 
-const ROW_HEIGHT = 73;
-const ITEM_PER_PAGE = 50;
-
 const STATUS_ICON = {
   [OPEN]: <InboxIcon />,
   [REOPEN]: <RestoreIcon />,
@@ -63,7 +54,7 @@ const mapItem = ({ owner, ...rest }) => ({
 });
 
 // HOOKS
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   container: {
     height: '100%',
     flexGrow: 1,
@@ -73,53 +64,25 @@ const useStyles = makeStyles((theme) => ({
     overflow: 'hidden',
     textOverflow: 'ellipsis',
   },
-  box: {
-    // 100% - margins - titlesHeight
-    height: `calc(100% - ${theme.spacing(1)}px - 67px)`,
-  },
-  list: {
-    height: '100%',
-    minHeight: 3 * ROW_HEIGHT,
-  },
   itemContainer: {
     width: '100%',
   },
 }));
 
 // COMPONENTS
-const Row = ({ list, isRowLoaded, classes, service, index, style, t }) => {
-  const item = useMemo(() => list[index] || {}, [index, list]);
-
-  const isLoaded = isRowLoaded({ index });
-  const { id, owner } = useMemo(() => item, [item]);
+const RequestRow = ({ item, classes, service, t }) => {
+  const { id, owner, updatedAt } = item;
+  const status = useMemo(() => getStatus(item), [item]);
+  const icon = useMemo(() => STATUS_ICON[status], [status]);
 
   const to = useMemo(() => !isNil(id) && generatePath(routes.dpo.service.requests.read, {
     databoxId: id,
     mainDomain: service.mainDomain,
   }), [id, service.mainDomain]);
 
-  if (!isLoaded) {
-    return (
-      <ListItem divider style={style}>
-        <ListItemAvatar>
-          <Skeleton height={40} width={40} variant="circle" />
-        </ListItemAvatar>
-        <Box mb={1}>
-          <Skeleton height={17} width={120} variant="text" />
-        </Box>
-        <Skeleton height={20} width={120} variant="text" />
-      </ListItem>
-    );
-  }
-
-  const status = getStatus(item);
-  const { updatedAt } = item;
-  const icon = STATUS_ICON[status];
 
   return (
     <ListItem
-      divider
-      style={style}
       component={Link}
       button
       to={to}
@@ -150,75 +113,44 @@ const Row = ({ list, isRowLoaded, classes, service, index, style, t }) => {
   );
 };
 
-Row.propTypes = {
-  list: PropTypes.arrayOf(PropTypes.object).isRequired,
-  isRowLoaded: PropTypes.func.isRequired,
+RequestRow.propTypes = {
+  item: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    owner: PropTypes.shape({
+      displayName: PropTypes.string.isRequired,
+      email: PropTypes.string.isRequired,
+    }).isRequired,
+    updatedAt: PropTypes.string.isRequired,
+  }).isRequired,
   classes: PropTypes.object.isRequired,
   service: PropTypes.object.isRequired,
-  index: PropTypes.number.isRequired,
-  style: PropTypes.object.isRequired,
   t: PropTypes.func.isRequired,
 };
 
-const RowWithTranslation = withTranslation('common__new')(Row);
+const TRequestRow = withTranslation('common__new')(RequestRow);
 
 function ServiceRequestsList({ appBarProps, service, t, isLoading, error }) {
   const classes = useStyles();
 
   const [list, setList] = useState([]);
-  const [offset, setOffset] = useState(0);
 
-  const [isNextPageLoading, setNextPageLoading] = useState(false);
-  const [hasNextPage, setHasNextPage] = useState(true);
+  const queryParams = useMemo(
+    () => ({
+      producerId: service.id,
+      withUsers: true,
+      statuses: OPEN,
+    }), [service],
+  );
 
-  const fetchList = useCallback((queryParams) => {
-    setNextPageLoading(true);
-
-    return API.use(ENDPOINTS.databoxes.list)
+  const fetchList = useCallback(
+    () => API.use(ENDPOINTS.databoxes.list)
       .build(null, null, objectToSnakeCase(queryParams))
       .send()
       .then((response) => {
-        setList((prevList) => [...prevList, ...response.map(mapItem)]);
-        if (response.length !== ITEM_PER_PAGE) { setHasNextPage(false); }
-      })
-      .finally(() => {
-        setOffset(queryParams.offset + ITEM_PER_PAGE);
-        setNextPageLoading(false);
-      });
-  }, []);
-
-  const loadNextPage = useCallback(() => {
-    const payload = {
-      producerId: service.id,
-      withUsers: true,
-      limit: ITEM_PER_PAGE,
-      offset,
-      statuses: OPEN,
-    };
-    return fetchList(payload);
-  }, [service, offset, fetchList]);
-
-  const rowCount = useMemo(
-    () => (hasNextPage ? list.length + 1 : list.length),
-    [hasNextPage, list],
+        setList(response.map(mapItem));
+      }),
+    [queryParams],
   );
-
-  // Only load 1 page of items at a time.
-  // Pass an empty callback to InfiniteLoader in case it asks us to load more than once.
-  const loadMoreRows = useMemo(
-    () => (isNextPageLoading ? noop : loadNextPage),
-    [isNextPageLoading, loadNextPage],
-  );
-
-  // Every row is loaded except for our loading indicator row.
-  const isRowLoaded = useCallback(
-    ({ index }) => !hasNextPage || index < list.length,
-    [hasNextPage, list],
-  );
-
-  const listProps = useMemo(() => ({
-    list, isRowLoaded, classes, service,
-  }), [list, isRowLoaded, classes, service]);
 
   const shouldFetch = useMemo(
     () => !isNil(service),
@@ -226,7 +158,7 @@ function ServiceRequestsList({ appBarProps, service, t, isLoading, error }) {
   );
 
   const { isFetching: isInternalFetching } = useFetchEffect(
-    loadNextPage,
+    fetchList,
     { shouldFetch },
   );
 
@@ -244,38 +176,14 @@ function ServiceRequestsList({ appBarProps, service, t, isLoading, error }) {
       state={state}
       appBarProps={appBarProps}
       navigationProps={{ showGoBack: false, noWrap: true }}
-      title={t('dpo_new:requests.list.title')}
+      title={t('dpo__new:requests.list.title')}
     >
       <Container maxWidth="md" className={classes.container}>
-        <BoxSection my={3} p={0} className={classes.box}>
+        <BoxSection my={3} p={0}>
           <List className={classes.list} disablePadding>
-            {list.length === 0 && <Empty />}
-            {list.length > 0 && (
-              <InfiniteLoader
-                isRowLoaded={isRowLoaded}
-                loadMoreRows={loadMoreRows}
-                rowCount={rowCount}
-              >
-                {({ onRowsRendered, registerChild }) => (
-                  <AutoSizer>
-                    {({ width, height }) => (
-                      <VirtualizedList
-                        width={width}
-                        height={height}
-                        tabIndex={null}
-                        ref={registerChild}
-                        onRowsRendered={onRowsRendered}
-                        rowHeight={ROW_HEIGHT}
-                        rowCount={rowCount}
-                        rowRenderer={(rowProps) => (
-                          <RowWithTranslation {...listProps} {...rowProps} />
-                        )}
-                      />
-                    )}
-                  </AutoSizer>
-                )}
-              </InfiniteLoader>
-            )}
+            {list.length === 0 ? <Empty /> : list.map((request) => (
+              <TRequestRow key={request.id} item={request} classes={classes} service={service} />
+            ))}
           </List>
         </BoxSection>
       </Container>
@@ -303,4 +211,4 @@ ServiceRequestsList.defaultProps = {
   error: null,
 };
 
-export default withTranslation(['dpo_new'])(ServiceRequestsList);
+export default withTranslation(['dpo__new'])(ServiceRequestsList);
