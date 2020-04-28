6934076531a9a8aeea204b38d0ffed140e3b527c
= <antoine.vadot@misakey.com>
2020-04-28 16:00:38 +0200

    fix: make export compatible with retina screens

--- src/packages/helpers/src/getScreenshotOfElement/index.js
+++ src/packages/helpers/src/getScreenshotOfElement/index.js
@@ -3,25 +3,10 @@ import html2canvas from 'html2canvas';
 
 export default (element) => {
   const { scrollHeight: height, scrollWidth: width } = element;
-
   return html2canvas(element, {
     width,
     height,
-    scale: 1,
     useCORS: true,
     allowTaint: true,
-  }).then((canvas) => {
-    const context = canvas.getContext('2d');
-    const imageData = context.getImageData(0, 0, width, height).data;
-    const outputCanvas = document.createElement('canvas');
-    const outputContext = outputCanvas.getContext('2d');
-    outputCanvas.width = width;
-    outputCanvas.height = height;
-
-    const idata = outputContext.createImageData(width, height);
-    idata.data.set(imageData);
-    outputContext.putImageData(idata, 0, 0);
-
-    return outputCanvas.toDataURL();
-  });
+  }).then((canvas) => canvas.toDataURL());
 };
