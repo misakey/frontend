fdc391b2951171d5b8defbe48f2b900c18200990
Claire Dhénin <claire.dhenin@misakey.com>
2020-09-10 11:15:31 +0200

    fix: Can't logout on confirm your identity screen

--- src/packages/auth/src/hooks/useSignOut/index.js
+++ src/packages/auth/src/hooks/useSignOut/index.js
@@ -2,26 +2,39 @@ import { useCallback } from 'react';
 
 import { signOut, setIsAuthenticated } from '@misakey/auth/store/actions/auth';
 import signOutBuilder from '@misakey/auth/builder/signOut';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import isFunction from '@misakey/helpers/isFunction';
+import { selectors as authSelectors } from '@misakey/auth/store/reducers/auth';
+
+// CONSTANTS
+const { isAuthenticated: IS_AUTHENTICATED_SELECTOR } = authSelectors;
 
 export default (userManager, onSuccess = null) => {
   const dispatch = useDispatch();
+  const isAuthenticated = useSelector(IS_AUTHENTICATED_SELECTOR);
+
+  const onSignOut = useCallback(
+    (e) => Promise.resolve(dispatch(signOut())).then(() => {
+      if (isFunction(onSuccess)) {
+        Promise.resolve(onSuccess(e));
+      }
+    }),
+    [dispatch, onSuccess],
+  );
 
   return useCallback(
-    (e) => {
-      // User to prevent component to refetch as token is still
-      // in store but have already been invalidateed in backend
-      dispatch(setIsAuthenticated(false));
-      return signOutBuilder()
-        .then(() => userManager.removeUser())
-        .then(() => Promise.resolve(dispatch(signOut(e))))
-        .then(() => {
-          if (isFunction(onSuccess)) {
-            Promise.resolve(onSuccess(e));
-          }
-        });
+    () => {
+      if (isAuthenticated) {
+        // User to prevent component to refetch as token is still
+        // in store but have already been invalidateed in backend
+        dispatch(setIsAuthenticated(false));
+        return signOutBuilder()
+          .then(() => userManager.removeUser())
+          .then(onSignOut);
+      }
+      // Clear identity and other fields if needed
+      return onSignOut();
     },
-    [dispatch, onSuccess, userManager],
+    [dispatch, isAuthenticated, onSignOut, userManager],
   );
 };
