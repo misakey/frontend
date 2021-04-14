65004241dde17c9018bb9cec1831ff8bac7302c7
Claire Dhénin <claire.dhenin@misakey.com>
2021-04-14 11:33:09 +0200

    fix: add migration for auth persisted reducer

--- src/packages/react/src/auth/store/reducers/index.js
+++ src/packages/react/src/auth/store/reducers/index.js
@@ -1,10 +1,28 @@
-import { persistReducer } from 'redux-persist';
+import { persistReducer, createMigrate } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
 import auth from '@misakey/react/auth/store/reducers/auth';
 import sso from '@misakey/react/auth/store/reducers/sso';
 
-const authPersistConfig = { key: 'auth', storage, whitelist: ['identity'], blacklist: [] };
+const migrations = {
+  // migration clear out identity state to trigger a re-fetch
+  // migration from -1 to 0 is for update from backend migration that replaced `hasAccount`
+  // by `hasCrypto`
+  0: (state) => ({
+    ...state,
+    identity: null,
+  })
+  ,
+};
+
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  version: 0,
+  whitelist: ['identity'],
+  blacklist: [],
+  migrate: createMigrate(migrations, { debug: false }),
+};
 
 export const authPersistedReducers = {
   auth: persistReducer(authPersistConfig, auth),
